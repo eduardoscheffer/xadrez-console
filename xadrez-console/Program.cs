25e76b4880017f0b91146ee7a34949fdea028bda
Eduardo Scheffer <eduardoscheffer91@gmail.com>
2023-07-25 19:42:58 -0300

    criado TabuleiroExceptions

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,7 +10,9 @@ class Program
 
         tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 0));
         tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(1, 3));
-        tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 4));
+        tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(2, 14));
+
+
 
         Tela.imprimirTabuleiro(tab);
 
--- xadrez-console/tabuleiro/Tabuleiro.cs
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -18,11 +18,33 @@ internal class Tabuleiro
     // metodo de Peca que retorna uma Peca do tabuleiro, ja que a propriedade eh private:
     public Peca peca(int linha, int coluna) => pecas[linha, coluna];
 
+    // sobecarga do metodo peca:
+    public Peca peca(Posicao pos) => pecas[pos.linha, pos.coluna];
+
+    // metodo que checa se existe uma peça em uma determinada posicao:
+    public bool existePeca(Posicao pos)
+    {
+        validarPosicao(pos); // primeiro faz a validação da posicao
+        return peca(pos) != null;
+    }
+
     // metodo que adiciona uma peca ao tabuleiro
     public void colocarPeca (Peca peca, Posicao posicao)
-    {   
+    {
+        if (existePeca(posicao))
+            throw new TabuleiroException("Já existe uma peça nessa posição!");
         // vai na matriz do campo pecas e coloca um objeto do tipo Peca:
         pecas[posicao.linha, posicao.coluna] = peca;
         peca.posicao = posicao;
     }
+
+    // metodo que checa se uma posicao é valida no tabuleiro:
+    public bool posicaoValida (Posicao pos) => (pos.linha < 0 || pos.linha > linhas || pos.coluna < 0 || pos.coluna > colunas) ? false : true;
+
+    public void validarPosicao (Posicao pos)
+    {
+        if (!posicaoValida(pos))
+            throw new TabuleiroException("Posicao Invalida!");
+    }
+
 }
